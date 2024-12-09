b7d125903d68a323bb2bfd4bd2c4b2d4cd735616
GivingHeartsInternational <144336851+GivingHeartsInternational@users.noreply.github.com>
2024-12-09 20:31:01 +0300

    Update create-stripe-session.js

--- pages/api/create-stripe-session.js
+++ pages/api/create-stripe-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')('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');
 
 
 const allowCors = (fn) => async (req, res) => {
